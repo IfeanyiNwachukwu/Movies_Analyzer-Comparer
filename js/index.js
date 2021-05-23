782b6f9cf6649664c9328f23b7e38c8ca6c8ee6f
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-05-23 14:53:55 +0100

    commit staged changes

--- js/index.js
+++ js/index.js
@@ -11,77 +11,62 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label for=""><b>Search For a Movie</b></label>
-<input type="text" class="input">
-<div class="dropdown">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results">
-            
-        </div>
-    </div>
-</div>
-
-`;
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-
-// first run timeoutId is falsy, setTimeout is ran returning a timeoutId, if there is another input event before 1000s, fetchData() is not run, rather and the process starts again until there is a 1s pause by the user, then fetchData runs. This is called Debouncing an Input. It means waiting for some time to pass after the last event to actually do something.
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-    if(!movies.length){
-        dropdown.classList.remove('is-active');
-        return;
-    }
-   
-    dropdown.classList.add('is-active');
-
-    resultsWrapper.innerHTML = ''; // everytime a fetch is done remove existing records  
-      
-    
-    for(let movie of movies){
-
-        const imgSrc = movie.Poster === 'N/A' ? ' ' : movie.Poster;
-        const option = document.createElement('a'); // an anchor tag
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
-        <img src="${imgSrc}"/>
-        ${movie.Title}
-        `;
-        option.addEventListener('click',() => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie.imdbID);
-        })
-        resultsWrapper.appendChild(option);
-    }
-}
-input.addEventListener('input',debounce(onInput));
-
-document.addEventListener('click',event => {
-    if(!root.contains(event.target)){
-        dropdown.classList.remove('is-active');
-    }
-});
 
 
 
-const onMovieSelect = async (movie) => {
-    const movieInfo =  await axios.get('http://www.omdbapi.com/',{
+
+const onMovieSelect = async (movieID) => {
+    const response =  await axios.get('http://www.omdbapi.com/',{
         params : {
             apikey : '5d060556',
-            i :  movie
+            i :  movieID
         }
     });
     // if(!movieInfo){
     //     return [];
     // }
-    console.log(movieInfo.data);
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
     
+}
+
+const movieTemplate = (movieDetail) => {
+
+    return `
+    <article class="media">
+    <figure class="media-left">
+        <p class="image">
+            <img src="${movieDetail.Poster}" alt="">
+        </p>
+    </figure>
+    <div class="media-content">
+        <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+    </div>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+</article>
+    `
 }
 
 
