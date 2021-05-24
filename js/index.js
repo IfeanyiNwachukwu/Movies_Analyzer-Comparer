3272496428d602b5f2865c6de3cf8185e44575a2
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-05-24 01:12:36 +0100

    commit all changes

--- js/index.js
+++ js/index.js
@@ -11,6 +11,65 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+createAutoComplete({
+root: document.querySelector('.autocomplete'),
+renderOption : (movie) => {
+    const imgSrc = movie.Poster === 'N/A' ? ' ' : movie.Poster;
+    return `
+    <img src="${imgSrc}"/>
+    ${movie.Title}
+    `
+  },
+  onOptionSelect : (movie) => {
+      onMovieSelect(movie);
+  },
+  inputValue : (movie) => {
+    return movie.Title;
+  },
+  fetchData : async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params : {
+            apikey: '5d060556',
+            s : searchTerm
+        }
+    });
+    if(response.data.Error){
+        return [];
+    }
+    return response.data.Search;
+}
+});
+
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption : (movie) => {
+        const imgSrc = movie.Poster === 'N/A' ? ' ' : movie.Poster;
+        return `
+        <img src="${imgSrc}"/>
+        ${movie.Title}
+        `
+      },
+      onOptionSelect : (movie) => {
+          onMovieSelect(movie);
+      },
+      inputValue : (movie) => {
+        return movie.Title;
+      },
+      fetchData : async (searchTerm) => {
+        const response = await axios.get('http://www.omdbapi.com/',{
+            params : {
+                apikey: '5d060556',
+                s : searchTerm
+            }
+        });
+        if(response.data.Error){
+            return [];
+        }
+        return response.data.Search;
+    }
+    });
+            
+        
 
 
 
